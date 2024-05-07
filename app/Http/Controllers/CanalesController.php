061971d29cb8d4c8c1b2e125676b10095df78869
jeortegaga <jeortegaga@gmail.com>
2024-05-07 21:03:45 +0000

    canales rutas woocommerce correcciones

--- app/Http/Controllers/CanalesController.php
+++ app/Http/Controllers/CanalesController.php
@@ -121,10 +121,11 @@ class CanalesController extends Controller
         $app_name = 'MultiTiendas';
         $scope = 'read_write';
         $user_id = Auth::user()->id;
-        $return_url = urlencode($urlbase.'/woocommerce/confirmed/'.$store->id);
-        $callback_url = urlencode($urlbase.'/woocommerce/add/'.$store->id);
-        $authorization_link = "$woocommerce_auth_url?app_name=$app_name&scope=$scope&user_id=$user_id&return_url=$return_url&callback_url=$callback_url";
-
+        $return_url = $urlbase.'/woocommerce/confirmed/'.$store->id;
+        $callback_url = $urlbase.'/woocommerce/add/'.$store->id;
+        
+        $authorization_link = $woocommerce_auth_url."?app_name=".$app_name."&scope=".$scope."&user_id=".$user_id."&return_url=".$return_url."&callback_url=".$callback_url;
+    
         // Retorna el enlace de autorización
         return response()->json(['authorization_link' => $authorization_link]);
     }
