ab70b222ef3dae23e9b32a6581cf3da4ab68c738
jeortegaga <jeortegaga@gmail.com>
2024-05-14 14:44:43 +0000

    ajustes de creacion usuarios test de mercadolibre

--- app/Http/Controllers/MercadolibreController.php
+++ app/Http/Controllers/MercadolibreController.php
@@ -80,7 +80,7 @@ class MercadolibreController extends Controller
     public function createTestUsers()
     {
         $canal = Canal::findOrFail(20);
-        $this->verificarYActualizarToken($canal);
+        dd($this->verificarYActualizarToken($canal));
         $accessToken = $canal->token; // Aquí deberías poner tu token de acceso
       
         $client = new Client([
